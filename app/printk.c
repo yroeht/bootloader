d8f46335fddf3466d3f69bc030af71d671bd40fb
Théophile Ranquet <theophile.ranquet@gmail.com>
2022-11-19 14:38:18 +0100

    app: use printk lib

--- app/printk.c
+++ app/printk.c
@@ -1,8 +1,9 @@
+#include "../lib/print.h"
+
 int main(void)
 {
-	char *msg = "Testing printk syscall\r\n";
-	asm volatile("mov $0, %%eax" : : : "eax");
-	asm volatile("mov %0, %%ebx" : : "m"(msg) : "ebx");
-	asm volatile("int $0x80");
+	char *msg = "Testing printk from app\r\n";
+	printk("%s", msg);
 	return 0;
 }
+
